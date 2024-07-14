3d30876fc9d6166f3373d3fe44bcd9114006e8ee
Ben Anderson <dataknut@icloud.com>
2024-07-14 15:54:55 +1200

    updated data url, updated data; re-ran

--- makefile.R
+++ makefile.R
@@ -1,4 +1,6 @@
- # makefile to run report
+# makefile to run report
+
+# refresh data from https://trap.nz/view/manage-trap-records/export.csv/169960?field_trap_trap_line_target_id_entityreference_filter%5B0%5D=190278&field_trap_record_date_value%5Bmin%5D%5Bdate%5D=&field_trap_record_date_value%5Bmax%5D%5Bdate%5D=&field_trap_record_bait_type_tid_entityreference_filter_op=in&field_trap_record_recorded_by_value=&field_tags_tid%5Btextfield%5D=&field_tags_tid%5Bvalue_field%5D=&title=&items_per_page=25
 
 library(here)
 
