30e9b8faccfbaebf2c2f20270c0e049aa9ce3c3d
Ben Anderson <dataknut@icloud.com>
2024-07-24 22:28:48 +1200

    added some xts testing

--- makefile.R
+++ makefile.R
@@ -30,3 +30,18 @@ rmarkdown::render(here::here(rmdFolder, "trapLineReport.Rmd"),
                   output_dir = paste0(outputFolder,"/", line),
                   output_format = "all" # should render all in .Rmd
 )
+
+# xts tests
+
+dt <- data.table::fread(latestDataFile)
+dt[, date_nz := lubridate::ymd_hm(date)]
+
+library(xts)
+xts_dt <- xts::as.xts(dt[, .(date_nz, code, strikes)])
+xts_dt['2024-06/']
+plot(xts_dt[,1],major.ticks='months',minor.ticks=FALSE,main=NULL,col=3)
+periodicity(xts_dt)
+to.monthly(xts_dt)
+
+
+
